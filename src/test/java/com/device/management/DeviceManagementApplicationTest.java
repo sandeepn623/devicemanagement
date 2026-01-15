5d1904dadff4bffd11615a869c49972ad9a9cb45
Sandeep Nanjundaswamy <sandeep@macbookpro.speedport.ip>
2026-01-15 07:21:00 +0100

    fix failing DeviceManagementApplicationTest

--- src/test/java/com/device/management/DeviceManagementApplicationTest.java
+++ src/test/java/com/device/management/DeviceManagementApplicationTest.java
@@ -1,15 +1,11 @@
 package com.device.management;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
+@SpringBootTest(properties = {
+        "spring.flyway.enabled=false",
+        "spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect"
 })
 public class DeviceManagementApplicationTest {
 
