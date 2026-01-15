c2d9d616ee369c3136789cfa2d4c4ce77c3c2f88
Sandeep Nanjundaswamy <sandeep@macbookpro.speedport.ip>
2026-01-15 02:16:12 +0100

    update basic DeviceManagementApplicationTest

--- src/test/java/com/device/management/DeviceManagementApplicationTest.java
+++ src/test/java/com/device/management/DeviceManagementApplicationTest.java
@@ -1,13 +1,20 @@
 package com.device.management;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeviceManagementApplicationTest {
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class DeviceManagementApplicationTest {
 
     @Test
     void contextLoads() {
+        // This test will pass if the Spring context loads successfully
     }
-
 }
