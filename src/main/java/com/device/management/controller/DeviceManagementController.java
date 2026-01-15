060b981ade74df82d56e7d146dcc87f01896911b
Sandeep Nanjundaswamy <sandeep@macbookpro.speedport.ip>
2026-01-15 04:22:26 +0100

    create a controller class for exposing rest apis for device managen=ment

---
+++ src/main/java/com/device/management/controller/DeviceManagementController.java
@@ -0,0 +1,9 @@
+package com.device.management.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devices")
+public class DeviceManagementController {
+}
